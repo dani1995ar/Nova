7f99a2a122278c4cfce1d9a370c6c8cf04456e6d
Jose AlVarez <Jose07alvarezm@gmail.com>
2023-04-08 10:22:04 -0500

    feat- First django app creation

--- Nova/Nova/settings.py
+++ Nova/Nova/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nova',
         'USER': 'root',
-        'PASSWORD': '12345',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
