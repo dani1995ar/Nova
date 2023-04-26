ac95ee36743db92c1b862569a1297805d8153ddb
Daniel Agudelo <daniel.agudelo@teaminternational.com>
2023-04-26 11:48:46 -0500

    chore: configure postgresql db and create readme

--- Nova/Nova/settings.py
+++ Nova/Nova/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Nova.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nova',
         'USER': 'root',
         'PASSWORD': '',
